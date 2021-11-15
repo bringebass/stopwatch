f4de555a7c292b66d53e643b8d49ccca5594d70e
bringebass <oyvindbringedal@gmail.com>
2021-11-15 19:44:48 +0100

    did it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useReducer, useEffect, useRef } from 'react'
 
 //this is a stopwatch
+// its built with the react hook useEffect and others
 
 function reducer(state, action) {
   if (action.type === 'run') {
